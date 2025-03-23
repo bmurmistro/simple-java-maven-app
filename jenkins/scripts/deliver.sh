2fb0540a87b272028471021e6a2c56809155b70b
Brandon Murray <bmurmistro@gmail.com>
2025-03-24 03:05:46 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -19,3 +19,9 @@ echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
 set +x
+
+echo 'The following command runs and outputs the execution of your Java'
+echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+set -x
+java -jar ../../target/${NAME}-${VERSION}.jar
+set +x
